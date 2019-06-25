5bb4679db9c33f0fcd4ded127bdd80f0332208f4
GRBurst <GRBurst@protonmail.com>
2019-06-25 17:29:20 +0200

    init

---
+++ Android.mk
@@ -0,0 +1,27 @@
+# DejaVu is licensed under the GPLv3. 
+# Copyright (C) 2017 - 2018 Tod Fitch
+#
+# This program is free software: you can redistribute it and/or modify 
+# it under the terms of the GNU General Public License as published by 
+# the Free Software Foundation, either version 3 of the License, or 
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but 
+# WITHOUT ANY WARRANTY; without even the implied warranty of 
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+# See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License 
+# along with this program. If not, see http://www.gnu.org/licenses/.
+#
+# The source code is available from 
+# https://github.com/n76/DejaVu
+#
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE       := DejaVu
+LOCAL_SRC_FILES    := app-release.apk
+LOCAL_MODULE_CLASS := APPS
+LOCAL_CERTIFICATE  := $(DEFAULT_SYSTEM_DEV_CERTIFICATE)
+include $(BUILD_PREBUILT)
